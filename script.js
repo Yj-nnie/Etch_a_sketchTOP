59fcebb9403f1f829133b41a4b804d5228990b67
Yanjun <yanjunliu9205@gmail.com>
2022-08-19 16:33:19 +0100

    added canvas

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 
 function initiate(e){
+    const canvas = document.createElement('div');
+    canvas.classList.add('my-canvas');
 
-    for(let idx = 1; idx <= 16; idx++){
-        
-        const container = document.createElement('div');    
-        container.classList.add('flexbox-container');
+        for(let idx = 1; idx <= 16; idx++){
+            
+            const flexcontainer = document.createElement('div');    
+            flexcontainer.classList.add('flexbox-container');
 
-            for(let i = 0; i<=16; i++) {
-            const square = document.createElement('div');
-            square.classList.add("flexbox-item", "flexbox-item-"+i);
-            container.appendChild(square);
-            }
-        containerLocation.appendChild(container);
-    }
+                for(let i = 0; i<=16; i++) {
+                const square = document.createElement('div');
+                square.classList.add("flexbox-item", "flexbox-item-"+i);
+                flexcontainer.appendChild(square);
+                }
+            canvas.appendChild(flexcontainer);
+        }
+    containerLocation.appendChild(canvas);
+}
+
+function changeColor(e){
+    const item = e.currentTarget;
+    item.style.color = "white";
 }
 
 
@@ -21,3 +29,5 @@ const toLoad = document.querySelector('button');
 const containerLocation = document.querySelector('body');
 toLoad.addEventListener('click', initiate)
 
+const hoveritems = document.querySelectorAll('.flex*')
+hoveritems.addEventListener('mouseenter', changeColor)
