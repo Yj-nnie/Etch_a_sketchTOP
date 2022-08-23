e6f1bb68a9b2165953bb9833c0e81600b4f58346
Yanjun <yanjunliu9205@gmail.com>
2022-08-23 15:55:22 +0100

    updated but incomplete changepixel

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function initialize(e){
    // canvas.classList.add('my-canvas');
     canvas.id="my-canvas";
     
-        for(let idx = 1; idx <= 18; idx++){
+        for(let idx = 1; idx <= initialPixels; idx++){
             
             const flexcontainer = document.createElement('div');    
             flexcontainer.classList.add('flexbox-container');
 
-                for(let i = 0; i<=18; i++) {
+                for(let i = 0; i<= initialPixels; i++) {
                 const square = document.createElement('div');
                 square.classList.add("flexbox-item");
                 //add my hover effect
@@ -26,32 +26,87 @@ function initialize(e){
 function changeColor(event){
 
         event.target.style.backgroundColor = "white";
+        // optional to leave the following in:
     /*    setTimeout(() => {
             event.target.style.backgroundColor= "";
           }, 15000);    */
-    
 }
 
-function clearBoard(){
-    console.log('hi')
+function clearBoard(event){
 
+    //revert button back to original color
+    event.target.style.backgroundColor="#EA4C89";
+    // grab boxes and recert back to original color
     const boxes = document.getElementsByClassName('flexbox-item')
     for(let i = 0; i < boxes.length; i++){
-        console.log(boxes[i]);
         boxes[i].style.backgroundColor="pink";
-    }
-    
+    } 
+
 }
+
+function updatePixel(event){
+
+
+    const mycanvas = document.getElementById('my-canvas');
+    const containers= document.getElementsByClassName('flexbox-container');
+    const items = document.getElementsByClassName('flexbox-item');
+    const canvasStyle = getComputedStyle(mycanvas);
+    const containerStyle = getComputedStyle(mycanvas);
+    const boxStyle = getComputedStyle(mycanvas);
+    
+    
+    var newPixel = prompt("enter size: ")
+    var oldPixel = canvasStyle.
+    var newContainerCount =  containers.clientHeight/newPixel
+    var newItemCount = items.clientwidth/newPixel
+    
+    console.log(containers.clientHeight)
+    console.log(items.clientwidth)
+
+    // remove nested divs and the canvas.
+    for(let j = containers.length-1; j >= 0; j--){
+
+        for(let i = items.length-1; i >= 0 ; i--){
+            console.log(items[i])
+            items[i].remove();
+        } 
+        containers[j].remove();
+    }
+
+    mycanvas.remove()
+
+   // initialPixels = newContainerCount;
+    initialize();
+
+}
+
 
 // create the board
 const toLoad = document.getElementById('chooseSize');
 const containerLocation = document.querySelector('body');
-//toLoad.addEventListener('click', initialize)
+//pixel globally accessible for creating board
+
+var initialPixels = 18
 
 initialize()
+
+//clear board function
 const toClear = document.getElementById('clear');
 toClear.addEventListener('click',clearBoard);
 
+//change pixel
+const changePixel= document.getElementById('pixel');
+changePixel.addEventListener('click', updatePixel);
+
+/* -- my canvas
+Largest width - 400
+largest height - 400
+
+--flexbox container - height 22
+-- flexbox container - width 400
+
+- smallest item 
+max width
 
 
-//size = window.prompt("how big do you want your board to be?");
+*/
