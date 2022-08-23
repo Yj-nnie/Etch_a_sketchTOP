c47a811ab440f30ca3b05721025abe44a1f44a3c
Yanjun <yanjunliu9205@gmail.com>
2022-08-23 10:09:33 +0100

    added clean board function

--- script.js
+++ script.js
@@ -1,46 +1,57 @@
 
-function initiate(e){
+function initialize(e){
+    
     const canvas = document.createElement('div');
    // canvas.classList.add('my-canvas');
     canvas.id="my-canvas";
-        for(let idx = 1; idx <= 16; idx++){
+    
+        for(let idx = 1; idx <= 18; idx++){
             
             const flexcontainer = document.createElement('div');    
             flexcontainer.classList.add('flexbox-container');
 
-                for(let i = 0; i<=16; i++) {
+                for(let i = 0; i<=18; i++) {
                 const square = document.createElement('div');
                 square.classList.add("flexbox-item");
-
                 //add my hover effect
-                square.addEventListener('mouseenter', function(event){
-                    event.target.style.backgroundColor = "purple";
-
-                    setTimeout(() => {
-                        event.target.style.backgroundColor= "";
-                      }, 1000);    
-                }, false);
-
+                square.addEventListener('mouseenter', changeColor, false);
                 flexcontainer.appendChild(square);
                 }
+
             canvas.appendChild(flexcontainer);
         }
     containerLocation.appendChild(canvas);
 }
 
-function changeColor(e){
-    const item = e.target;
-    item.style.color = "white";
+function changeColor(event){
+
+        event.target.style.backgroundColor = "white";
+    /*    setTimeout(() => {
+            event.target.style.backgroundColor= "";
+          }, 15000);    */
+    
 }
 
+function clearBoard(){
+    console.log('hi')
+
+    const boxes = document.getElementsByClassName('flexbox-item')
+    for(let i = 0; i < boxes.length; i++){
+        console.log(boxes[i]);
+        boxes[i].style.backgroundColor="pink";
+    }
+    
+}
 
 // create the board
-const toLoad = document.querySelector('button');
+const toLoad = document.getElementById('chooseSize');
 const containerLocation = document.querySelector('body');
-toLoad.addEventListener('click', initiate)
-/*
-    var re = new RegExp ('flexbox-item flexbox-item-[0-9]')
-    const hoveritems = document.getElementById('my-canvas').getElementsByClassName('flexbox-container').getElementsByClassName('flexbox-item"');
-    console.log(hoveritems)
-    console.log(hoveritems.namedItem)
-//hoveritems.addEventListener('mouseover', changeColor) */
+//toLoad.addEventListener('click', initialize)
+
+initialize()
+const toClear = document.getElementById('clear');
+toClear.addEventListener('click',clearBoard);
+
+
+
+//size = window.prompt("how big do you want your board to be?");
